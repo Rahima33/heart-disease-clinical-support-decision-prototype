589ff29b2c34636f9f3f4417c28e0b92ed06e3e0
Rahima <rahimaejaz33@gmail.com>
2026-07-03 12:56:25 +0500

    update

--- app.py
+++ app.py
@@ -222,6 +222,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "https://heart-disease-clinical-support-deci.vercel.app",
+        "https://cardioriskapp.vercel.app",
     ],
     allow_methods=["*"],
     allow_headers=["*"],
